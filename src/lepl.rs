4c167a29f8563c4abba03d1baa51edb58ff80691
keiya01 <keiya.s.0210@gmail.com>
2020-11-22 11:09:36 +0900

    feat: impl lepl

---
+++ src/lepl.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Write};
+
+use crate::{lexer, token};
+
+fn log_token(buf: String) {
+  let mut l = lexer::Lexer::new(&buf);
+  loop {
+    let t = l.next_token();
+    if let token::Token::EOF = t {
+      break;
+    }
+    println!("{:?}", t);
+  }
+}
+
+pub fn start() {
+  loop {
+    print!("> ");
+    io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut buf).unwrap();
+    log_token(buf);
+  }
+}

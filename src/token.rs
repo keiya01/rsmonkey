561cdfe45ca7aafad665360855e5dee3df340874
keiya01 <keiya.s.0210@gmail.com>
2020-11-24 11:39:02 +0900

    feat: impl display trait for Token enum

--- src/token.rs
+++ src/token.rs
@@ -1,6 +1,7 @@
 use std::cmp::PartialEq;
+use std::fmt;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token {
   ILLEGAL,
   EOF,
@@ -39,4 +40,48 @@ pub enum Token {
   IF,
   ELSE,
   RETURN,
+}
+
+impl fmt::Display for Token {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Token::ILLEGAL => write!(f, "ILLEGAL"),
+      Token::EOF => write!(f, "EOF"),
+
+      // 識別子 + リテラル
+      Token::IDENT(s) => write!(f, "IDENT({})", s),
+      Token::INT(i) => write!(f, "INT({})", i),
+      
+      // 演算子
+      Token::ASSIGN => write!(f, "ASSIGN"),
+      Token::PLUS => write!(f, "PLUS"),
+      Token::MINUS => write!(f, "MINUS"),
+      Token::BANG => write!(f, "BANG"),
+      Token::ASTERISK => write!(f, "ASTERISK"),
+      Token::SLASH => write!(f, "SLASH"),
+    
+      Token::LT => write!(f, "LT"),
+      Token::GT => write!(f, "GT"),
+      Token::EQ => write!(f, "EQ"),
+      Token::NotEq => write!(f, "NotEq"),
+      
+      // デリミタ
+      Token::COMMA => write!(f, "COMMA"),
+      Token::SEMICOLON => write!(f, "SEMICOLON"),
+      
+      Token::LPAREN => write!(f, "LPAREN"),
+      Token::RPAREN => write!(f, "RPAREN"),
+      Token::LBRACE => write!(f, "LBRACE"),
+      Token::RBRACE => write!(f, "RBRACE"),
+      
+      // キーワード
+      Token::FUNCTION => write!(f, "FUNCTION"),
+      Token::LET => write!(f, "LET"),
+      Token::TRUE => write!(f, "TRUE"),
+      Token::FALSE => write!(f, "FALSE"),
+      Token::IF => write!(f, "IF"),
+      Token::ELSE => write!(f, "ELSE"),
+      Token::RETURN => write!(f, "RETURN"),
+    }
+  }
 }

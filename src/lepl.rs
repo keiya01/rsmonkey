d8bbfdfe6bafc7953823b4b3e50c77fb2808a0ac
keiya01 <keiya.s.0210@gmail.com>
2020-11-28 19:12:38 +0900

    fix: fix lepl to output evaluated result

--- src/lepl.rs
+++ src/lepl.rs
@@ -1,17 +1,17 @@
 use std::io::{self, Write};
 
-use crate::{lexer, parser};
+use crate::{lexer, parser, evaluator};
 
 fn log_token(buf: String) {
   let l = lexer::Lexer::new(buf);
   let mut p = parser::Parser::new(l);
   let program = p.parse_program();
-
+  
   if !p.check_parse_errors() {
     return;
   }
-
-  println!("{}", program);
+  
+  println!("{}", evaluator::eval(program));
 }
 
 pub fn start() {
